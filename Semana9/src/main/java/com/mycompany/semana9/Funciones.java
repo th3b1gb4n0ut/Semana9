b79e85e603ae817ab31d93b4bf9ca06d46f506f1
Wilfran Isai Lazaro Barrientos <41808497+th3b1gb4n0ut@users.noreply.github.com>
2022-05-22 06:04:19 -0500

    Add files via upload

---
+++ Semana9/src/main/java/com/mycompany/semana9/Funciones.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.semana9;
+
+/**
+ *
+ * @author user
+ */
+public class Funciones {
+    
+    public String[] s1(String[] s1, int v, int e){
+        
+  
+        
+        String[] ns1 = null;
+           
+     
+    
+        
+        
+     String rec  = s1[0].substring(v, e);
+      ns1[] String = null;
+       
+     String[5] = ns1;
+     
+     
+     for(String cut:s1){
+            int i = 0;
+        
+        ns1[i] = cut.substring(v, e);
+         
+     }
+        
+        
+     
+         
+ 
+  
---
+++ Semana9/src/main/java/com/mycompany/semana9/Leer_3_arreglos.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.semana9;
+
+/**
+ *
+ * @author user
+ */
+public class Leer_3_arreglos extends Funciones {
+    
+     public static void main(String[] args){
+         
+         System.out.println("arreglo:  " arreglo[0, 2]);
+     }
+}
